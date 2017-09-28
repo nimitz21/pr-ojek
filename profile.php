ed8d910a2961572f767506aed769a65cf8f24b8d
Vincent Endrahadi <13515117@std.stei.itb.ac.id>
2017-09-29 00:33:24 +0700

    add profile draft page and css

---
+++ profile.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Profile</title>
+
+  <link rel="stylesheet" href="css/profile.css">
+</head>
+
+
+<body>
+
+  <div class="nav-header">
+    <ul class="header">
+      <li class="nav-title">
+        <p id="titlenav">
+          <span id="pr">
+            PR
+          </span>-
+          <span id="ojek">
+            OJEK
+          </span>
+        </p>
+
+        <p id="nav-uname">Hi, </p>
+      </li>
+
+      <li class="nav-description">
+        <p id="nav-desc">
+          wush... wush... ngeeeeengg...
+        </p>
+        <a href="/logout.php" class="logout-link">
+          Logout
+        </a>
+
+      </li>
+    </ul>
+  </div>
+<div class="nav-body">
+  <ul class="navigation">
+    <li id="order">
+      <a href="/order.php">ORDER</a>
+    </li>
+    <li id="history">
+      <a href="/history.php">HISTORY</a>
+    </li>
+    <li id="profile">
+      <a href="/profile.php">MY PROFILE</a>
+    </li>
+  </ul>
+</div>
+
+
+<div class="profile">
+  <div class="prof-header">
+    <p id="myprofile">
+      My Profile
+    </p>
+    <img id="editpic" src="storage/images/editpic.jpg" alt="Edit">
+  </div>
+</div>
+
+<div class="info">
+  <img id="profpic" src="storage/images/profpic.jpg" alt="Profile Picture">
+</div>
+
+
+
+
+</body>
+
+</html>

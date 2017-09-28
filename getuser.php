46b2897158f26b1e57582276408476de0394b05b
Vincent Endrahadi <13515117@std.stei.itb.ac.id>
2017-09-29 03:00:04 +0700

    add file to fetch logged user.

---
+++ getuser.php
@@ -0,0 +1,20 @@
+<?php 
+
+$actid = $_SESSION["user_id"];
+
+$query = "SELECT * FROM users WHERE ID=" . $actid;
+$results = $db->query($query);
+
+if($results->num_rows > 0) {
+  $result = $results->fetch_assoc();
+}
+
+$ratequery = "SELECT avg(rating) as avgrate, count(rating) as votes FROM orders WHERE user_id=" . $actid;
+$ratings = $db->query($ratequery);
+
+if($ratings->num_rows > 0) {
+  $rating = $ratings->fetch_assoc();
+}
+
+
+?>
